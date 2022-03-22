12a1820a5636d3ffba6153edf619b869e08c98fb
Ryan DuCharme <ryan.ducharme77@gmail.com>
2022-03-22 01:51:04 -0400

    update

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/////////////////
+// either randomply place each piece and then have the user move them in the 
+// cardinal directions directly, or have an input box next to each ship wiht x,y coords to place the ship at
+////////
 let canvas = document.querySelector('#playerOneCanvas');
 let context = canvas.getContext('2d');
 let debugOne = document.getElementById('debugP1');
@@ -9,16 +13,6 @@ let checkHitButton = document.getElementById('checkHitButton');
 let playerOneBoard = createBoard(10);
 let playerTwoBoard = createBoard(10);
 let count = 0;
-
-
-let ship1 = new Ship(
-    [
-        [0,1,0],
-        [0,1,0],
-        [0,1,0]
-    ]
-);
-
 
 let player = {
     ships: [],
@@ -32,14 +26,165 @@ let gameBoard = {
     players: [],
 
 }
+
+let verticalShip = new Ship(
+    [
+        [1],
+        [1],
+        [1]
+    ]
+);
+
+let horizontalShip = new Ship(
+    [
+        [1, 1, 1]
+    ]
+);
+
+let comboShip = new Ship(
+    [
+        [1, 1, 1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ]
+);
+
 // playerOneAddShip.onclick = placeShip(playerOneBoard, Math.floor(Math.random() * 10), Math.floor(Math.random() * 10));
 playerOneAddShip.addEventListener('click', placeRandomShip);
-checkHitButton.addEventListener('click', detectHit)
+checkHitButton.addEventListener('click', detectHit);
 
-function Ship(layout){
+
+placeShip(playerOneBoard, verticalShip.layout, 0, 1);
+debugOne.innerHTML = playerOneBoard.join('\n');
+console.log(verticalShip.layout.length);
+console.log(verticalShip.layout[0].length);
+
+
+
+function Ship(layout) {
     this.layout = layout;
 }
 
+function placeShip(board, ship, chosenX, chosenY) {
+
+    let pieceConflict = false;
+
+    //check valid placement
+    //check if the desired coords are in bounds
+    if ((chosenX < 10 && chosenY < 10)) {
+        if (calcShipBaseAxis(ship) === 'horizontal') {
+            for (let shipX = 0; shipX < ship[0].length; shipX++) {
+                //check to make sure the piece will fit in desired location
+                if (chosenX + ship[0].length > 10 || chosenY + ship.length > 10) {
+                    //check the piece isnt conflicting with another ship
+                    //          X               Y
+                    console.log(`The piece will be out of bounds if placed at X:${chosenX} Y:${chosenY}`);
+                    pieceConflict = true;
+                } else if (board[chosenY + 0][chosenX + shipX] === 1) {
+                    console.log(`Overlapping pieces at X:${chosenX} Y:${chosenY}`);
+                    pieceConflict = true;
+                }
+            }
+        } else if (calcShipBaseAxis(ship) === 'vertical') {
+            for (let shipX = 0; shipX < ship[0].length; shipX++) {
+                for (let shipY = 0; shipY < ship.length; shipY++) {
+                    if (chosenX + ship[0].length > 10 || chosenY + ship.length > 10) {
+                        console.log(`The piece will be out of bounds if placed at X:${chosenX} Y:${chosenY}`);
+                        pieceConflict = true;
+                    } else if (board[chosenY + shipY][chosenX + 0] === 1) {
+                        console.log(`Overlapping pieces at X:${chosenX} Y:${chosenY}`);
+                        pieceConflict = true;
+                    }
+                }
+            }
+        } else if (calcShipBaseAxis(ship) === 'combo') {
+            //todo
+            console.log('combo');
+        }
+    } else {
+        console.log(`Desired location X:${chosenX} Y: ${chosenY} is out of bounds`);
+        pieceConflict = true;
+    }
+
+
+    if (pieceConflict == false) {
+        for (let shipX = 0; shipX < ship[0].length; shipX++) {
+            for (let shipY = 0; shipY < ship.length; shipY++) {
+                board[chosenY + shipY][chosenX + shipX] = 1;
+            }
+        }
+    }
+    debugOne.innerHTML = playerOneBoard.join('\n');
+}
+
+
+function detectHit(board, boardX, boardY) {
+    let checkHit = document.getElementById('checkHit').value;
+    let parsed = checkHit.split(',');
+    if (playerOneBoard[parsed[1]][parsed[0]] === 1) {
+        console.log('hit');
+        playerOneBoard[parsed[1]][parsed[0]]++;
+    }
+    debugOne.innerHTML = playerOneBoard.join('\n');
+}
+
+function rotateShip(shipLayout) {
+    const N = shipLayout.length;
+    // Consider all squares one by one
+    for (let x = 0; x < N / 2; x++) {
+
+        // Consider elements in group
+        // of 4 in current square
+        for (let y = x; y < N - x - 1; y++) {
+
+            // Store current cell in
+            // temp variable
+            let temp = shipLayout[x][y];
+
+            // Move values from right to top
+            shipLayout[x][y] = shipLayout[y][N - 1 - x];
+
+            // Move values from bottom to right
+            shipLayout[y][N - 1 - x]
+                = shipLayout[N - 1 - x][N - 1 - y];
+
+            // Move values from left to bottom
+            shipLayout[N - 1 - x][N - 1 - y] = shipLayout[N - 1 - y][x];
+
+            // Assign temp to left
+            shipLayout[N - 1 - y][x] = temp;
+        }
+    }
+    return shipLayout;
+}
+
+function placeRandomShip() {
+    placeShip(playerOneBoard, horizontalShip.layout, 0, 0);
+}
+
+function calcShipBaseAxis(shipLayout) {
+    let horizontal = false;
+    let vertical = false;
+    let combo = false;
+
+    // console.log(shipLayout.length);
+    // console.log(shipLayout[0].length);
+
+    if (shipLayout.length === 1) {
+        horizontal = true;
+    }
+    if (shipLayout[0].length === 1) {
+        vertical = true;
+    }
+    if (shipLayout.length > 1 && shipLayout[0].length > 1) {
+        combo = true;
+        return 'combo';
+    } else if (horizontal) {
+        return 'horizontal';
+    } else if (vertical) {
+        return 'vertical';
+    }
+}
 
 function createBoard(boardSize) {
     let board = [];
@@ -52,113 +197,5 @@ function createBoard(boardSize) {
     }
     return board;
 }
-
-function placeShip(board, ship, row, col) {
-
-    
-   
-    let chosenY = row;
-    let chosenX = col;
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //CHECK THE SPACE YOU WANT TO PUT THE PIECE IN FULLY BEFORE ADDING THE PIECE. IF THERE ARE TWO OVERLAPPING PIECES THEY WILL STILL BE ADDED TO THE BOARD IN THIS IMPLEMENTATION
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    //check the desired x y coords to see if there is already a ship there. if there is 
-    let invalidPlacementCount = 0;
-    if (chosenY <= board.length - ship.length && chosenX <= board.length - ship.length) {
-        for (let shipX = 0; shipX < ship.length; shipX++) {
-            for (let shipY = 0; shipY < ship.length; shipY++) {
-                //check if the space where the ship is going to be placed is empty
-                if (board[chosenY + shipX][chosenX + shipY] === 1) {
-                    invalidPlacementCount++;
-                    console.log(`capn.. theres a ship there\'s ${invalidPlacementCount} ship(s) there...` );
-                }
-            }
-        }
-    } else {
-        console.log('cant fit.. phalice too large');
-    }
-
-    if (invalidPlacementCount === 0) {
-        for (let shipX = 0; shipX < ship.length; shipX++) {
-            for (let shipY = 0; shipY < ship.length; shipY++) {
-                if (ship[shipX][shipY] === 1 && board[chosenY + shipX][chosenX + shipY] === 0) {
-                    board[chosenY + shipX][chosenX + shipY] = 1;
-                } else if (ship[shipX][shipY] === 0) {
-
-                }
-            }
-        }
-    }
-    debugOne.innerHTML = playerOneBoard.join('\n');
-    validPlacement = false;
-}
-
-function placeRandomShip() {
-    placeShip(playerOneBoard, ship1.layout, 0, 0);
-    
-    // ship1.layout = rotateShip(ship1.layout);
-    // console.log(ship1.layout);
-}
-
-function detectHit(board, row, col) {
-    let checkHit = document.getElementById('checkHit').value;
-    let parsed = checkHit.split(',');
-    if (playerOneBoard[parsed[0]][parsed[1]] === 1) {
-        console.log('hit');
-        playerOneBoard[parsed[0]][parsed[1]]++;
-    }
-    debugOne.innerHTML = playerOneBoard.join('\n');
-}
-
-function update() {
-    //check turn
-
-    //do player logic
-
-    //update player data
-}
-
-function draw(graphicsContext) {
-    //draw the players board
-
-}
-
-
-function rotateShip(shipLayout)
-    {
-        const N = shipLayout.length;
-        // Consider all squares one by one
-        for (let x = 0; x < N / 2; x++)
-        {
-         
-            // Consider elements in group
-            // of 4 in current square
-            for (let y = x; y < N - x - 1; y++)
-            {
-             
-                // Store current cell in
-                // temp variable
-                let temp = shipLayout[x][y];
-   
-                // Move values from right to top
-                shipLayout[x][y] = shipLayout[y][N - 1 - x];
-   
-                // Move values from bottom to right
-                shipLayout[y][N - 1 - x]
-                    = shipLayout[N - 1 - x][N - 1 - y];
-   
-                // Move values from left to bottom
-                shipLayout[N - 1 - x][N - 1 - y] = shipLayout[N - 1 - y][x];
-   
-                // Assign temp to left
-                shipLayout[N - 1 - y][x] = temp;
-            }
-        }
-        return shipLayout;
-}
-
-placeShip(playerOneBoard, ship1.layout, 0, 0)
-debugOne.innerHTML = playerOneBoard.join('\n');
 
 
