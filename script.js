6faebdcf6b47fb6f8d9a76016da309915c6ff069
Ryan DuCharme <ryan.ducharme77@gmail.com>
2022-04-03 19:25:35 -0400

    Placement updates

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let playerTwoGameBoardCtx = playerTwoGameBoard.getContext('2d');
 let playerTwoGuessBoard = document.getElementById('playerTwoGuessBoard');
 let playerTwoGuessBoardCtx = playerTwoGuessBoard.getContext('2d');
 
+let mouseCoordElem  = document.getElementById('mouse');
+
 //#endregion
 
 //#region Debug elements
@@ -32,6 +34,12 @@ function Guess(x, y) {
     this.x = x;
     this.y = y;
     this.hit = false;
+}
+
+let GameState = {
+    shipPlacement: true,
+    p1Turn: false,
+    p2Turn: false
 }
 
 function Game() {
@@ -63,15 +71,11 @@ function Game() {
         //return name of ship on given x, y gameboard coords
         var hitShip;
         let isHit = false;
-        // console.log(`X:${x} Y:${y}`);
         playerToHit.fleet.forEach(
             function (ship) {
 
                 if ((x >= ship.x && x < ship.x + ship.layout[0].length) && (y === ship.y)) {
                     ship.layout[0][x - ship.x + ship.layout[0].length - 1] = 2;
-                    // console.log(ship.layout[y][x - ship.layout[0].length]);
-                    // playerToHit.gameBoard[y][x] = 2;
-                    
                     isHit = true;
                     hitShip = ship;
                 }
@@ -79,9 +83,7 @@ function Game() {
 
         //CHANGE THIS T NOT CHANGE THE BOARD, MAKE FUNCTION TO REWRITE THE BOARD BY READING ALL BOAT ARRAYS
         if (isHit && playerToHit.gameBoard[y][x] == 1) {
-            // console.log(`${initiator.name} hit ${playerToHit.name}'s ${hitShip.name} at ${x},${y}`);
-            // console.log(hitShip.layout[y - hitShip.y][x - hitShip.x]);
-            // console.log(hitShip.layout);
+          
             playerToHit.gameBoard[y][x] = 2;
             initiator.guessBoard[y][x] = 'X';
             console.log('hit');
@@ -97,10 +99,13 @@ function Player(name, gameBoardContext, guessBoardContext) {
     this.name = name;
     this.gameBoard = createBoard(10);
     this.guessBoard = createBoard(10);
+    this.tempGameBoard = createBoard(10);
     this.gameBoardContext = gameBoardContext;
     this.guessBoardContext = guessBoardContext;
     // this.context = '2d';
     this.hitCount = 0;
+    this.MousePos = [];
+    this.currentShipIndex = 0;
     this.fleet =
         [
             new Ship(
@@ -122,20 +127,31 @@ function Player(name, gameBoardContext, guessBoardContext) {
     this.turnCount = 0;
     this.placeShip = function (ship, chosenX, chosenY) {
         let pieceConflict = false;
+        
         let shipLayout = ship.layout;
         //check valid placement
         //check if the desired coords are in bounds
+
+        // clear out temp board
+        for (let i = 0; i < this.tempGameBoard.length; i++) {
+            for (let j = 0; j < this.tempGameBoard.length; j++) {
+                if (this.gameBoard[j][i] === -1) {
+                    this.gameBoard[j][i] = 0
+                }
+
+            }
+        }
+
         if ((chosenX < 10 && chosenY < 10)) {
             if (ship.calcShipBaseAxis() === 'horizontal') {
                 for (let shipX = 0; shipX < shipLayout[0].length; shipX++) {
                     //check to make sure the piece will fit in desired location
                     if (chosenX + shipLayout[0].length > 10 || chosenY + shipLayout.length > 10) {
                         //check the piece isnt conflicting with another ship
-                        //          X               Y
-                        console.log(`The piece will be out of bounds if placed at X:${chosenX} Y:${chosenY}`);
+                        
+                        
                         pieceConflict = true;
                     } else if (this.gameBoard[chosenY + 0][chosenX + shipX] === 1) {
-                        console.log(`Overlapping pieces at X:${chosenX} Y:${chosenY}`);
                         pieceConflict = true;
                     }
                 }
@@ -146,17 +162,14 @@ function Player(name, gameBoardContext, guessBoardContext) {
                             console.log(`The piece will be out of bounds if placed at X:${chosenX} Y:${chosenY}`);
                             pieceConflict = true;
                         } else if (this.gameBoard[chosenY + shipY][chosenX + 0] === 1) {
-                            console.log(`Overlapping pieces at X:${chosenX} Y:${chosenY}`);
                             pieceConflict = true;
                         }
                     }
                 }
             } else if (calcShipBaseAxis(ship) === 'combo') {
                 //todo
-                console.log('combo');
             }
         } else {
-            console.log(`Desired location X:${chosenX} Y: ${chosenY} is out of bounds`);
             pieceConflict = true;
         }
 
@@ -166,12 +179,17 @@ function Player(name, gameBoardContext, guessBoardContext) {
                 for (let shipY = 0; shipY < shipLayout.length; shipY++) {
                     ship.x = chosenX;
                     ship.y = chosenY;
-                    // this.gameBoard[chosenY + shipY][chosenX + shipX] = 1;
-                    this.gameBoard[chosenY + shipY][chosenX + shipX] = ship.layout[shipY][shipX];
+                    if (ship.canPlace === true) {
+                        this.gameBoard[chosenY + shipY][chosenX + shipX] = ship.layout[shipY][shipX];
+                        ship.isPlaced = true;
+                        
+                    } else {
+                        this.gameBoard[chosenY + shipY][chosenX + shipX] = -1;
+                    }
                 }
             }
+            ship.canPlace = false;
         }
-
     };
 }
 
@@ -181,6 +199,8 @@ function Ship(name, layout) {
     this.health = layout[0].length;
     this.x = undefined;
     this.y = undefined;
+    this.canPlace = false;
+    this.isPlaced = false;
     this.calcShipBaseAxis = function () {
         let horizontal = false;
         let vertical = false;
@@ -203,7 +223,6 @@ function Ship(name, layout) {
     }
 
 }
-
 function createBoard(boardSize) {
     let board = [];
     for (let i = 0; i < boardSize; i++) {
@@ -215,21 +234,18 @@ function createBoard(boardSize) {
     }
     return board;
 }
-
 function p1Submit() {
     //check hit and update
     let coords = p1GuessInput.value.split(',');
     game.checkHit(game.playerOne, game.playerTwo, parseInt(coords[0]), parseInt(coords[1]));
     drawText();
 }
-
 function p2Submit() {
     //check hit and update
     let coords = p2GuessInput.value.split(',');
     game.checkHit(game.playerTwo, game.playerOne, parseInt(coords[0]), parseInt(coords[1]));
     drawText();
 }
-
 //draw
 function drawText() {
     debugOne.value = game.playerOne.gameBoard.join('\n').replace(/0/g, ' ');
@@ -239,45 +255,18 @@ function drawText() {
     debugTwo.value = game.playerTwo.gameBoard.join('\n').replace(/0/g, ' ');
     debugTwoGuesses.value = game.playerTwo.guessBoard.join('\n').replace(/0/g, ' ');
 }
-
-
 function getMousePosition(canvas, event) {
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
-    // console.log("Coordinate x: " + x,
-        // "Coordinate y: " + y);
-
-
+    
     x = Math.floor(x / 50);
     y = Math.floor(y / 50);
-    // console.log(`${x}, ${y}`);
 
     let position = [x, y];
     return position;
-    // game.playerOne.gameBoard[y][x] = 5;
-    // game.checkHit(game.playerOne, game.playerTwo, x, y);
-    // ctx.fillStyle = 'lime'
-    // ctx.fillRect(x * 50, y * 50, 50, 50);
+    
 }
-
-
-function update() {
-    //check input {
-    // mouse move
-    // mouse click
-    // }
-}
-let frameCount = 0;
-function gameLoop(timeStamp) {
-    draw();
-    // game.playerOne.gameBoardContext.clearRect(0,0, 500,500)
-    // Keep requesting new frames
-    frameCount++;
-    // console.log(frameCount);
-    window.requestAnimationFrame(gameLoop);
-}
-
 function drawBoard(player, cellSize, rows, cols) {
     // player.gameBoardContext.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -286,8 +275,8 @@ function drawBoard(player, cellSize, rows, cols) {
             let x = i * cellSize;
             let y = j * cellSize
 
-            player.gameBoardContext.fillStyle = 'white';
-            player.guessBoardContext.fillStyle = 'white';
+            player.gameBoardContext.fillStyle = 'blue';
+            player.guessBoardContext.fillStyle = 'lightgray';
 
             if ((i + j) % 2 === 0) {
 
@@ -298,16 +287,16 @@ function drawBoard(player, cellSize, rows, cols) {
             player.gameBoardContext.fillRect(x, y, cellSize, cellSize);
             player.guessBoardContext.fillRect(x, y, cellSize, cellSize);
 
+
+
             player.gameBoardContext.fillStyle = "black";
-            player.gameBoardContext.font = "12px Arial";
-            player.gameBoardContext.fillText(`${i},${j}`, x + 15, y + 30);
+            if (player.gameBoard[j][i] == -1) {
+                player.gameBoardContext.fillStyle = "lime";
+                player.gameBoardContext.fillRect(x, y, cellSize, cellSize);
 
-
-            player.guessBoardContext.fillStyle = "black";
-            player.guessBoardContext.font = "12px Arial";
-            player.guessBoardContext.fillText(`${i},${j}`, x + 15, y + 30);
-
+            }
             if (player.gameBoard[j][i] >= 1) {
+                player.guessBoardContext.fillStyle = "black";
                 player.gameBoardContext.fillRect(x, y, cellSize, cellSize);
 
             }
@@ -335,21 +324,47 @@ function drawBoard(player, cellSize, rows, cols) {
                 player.gameBoardContext.fillRect(x, y, cellSize, cellSize);
             }
 
+            player.gameBoardContext.fillStyle = "white";
+            player.gameBoardContext.font = "16px Arial";
+            player.gameBoardContext.fillText(`${i},${j}`, x + 15, y + 30);
+
+            player.guessBoardContext.fillStyle = "white";
+            player.guessBoardContext.font = "16px Arial";
+            player.guessBoardContext.fillText(`${i},${j}`, x + 15, y + 30);
         }
     }
 
 }
 
+
+function update() {
+    if (GameState.shipPlacement === true) {
+        if(game.playerOne.currentShipIndex <= 4) {
+            game.playerOne.placeShip(game.playerOne.fleet[game.playerOne.currentShipIndex], game.playerOne.MousePos[0], game.playerOne.MousePos[1]);
+            if(game.playerOne.fleet[game.playerOne.currentShipIndex].isPlaced === true ) {
+                game.playerOne.currentShipIndex++;
+            }
+        }
+    }
+}
+
 function draw() {
-    // game.playerOne.gameBoardContext.
     drawBoard(game.playerOne, 50, 10, 10);
     drawBoard(game.playerTwo, 50, 10, 10);
 }
 
+function gameLoop(timeStamp) {
+    update();
+    draw();
+    // Keep requesting new frames    
+
+    window.requestAnimationFrame(gameLoop);
+}
 
 playerOneGameBoard.addEventListener("mousedown", function (e) {
     getMousePosition(playerOneGameBoard, e);
-    // game.checkHit(game.playerTwo, game.playerTwo, position[0], position[1]);
+    game.playerOne.fleet[game.playerOne.currentShipIndex].canPlace = true;
+    
 });
 
 playerOneGuessBoard.addEventListener("mousedown", function (e) {
@@ -359,30 +374,28 @@ playerOneGuessBoard.addEventListener("mousedown", function (e) {
     drawText();
 });
 
-
+playerOneGameBoard.addEventListener("mousemove", function (e) {
+    let position = getMousePosition(playerOneGameBoard, e);
+    game.playerOne.MousePos = position;
+    mouseCoordElem.textContent = position;
+    drawText();
+});
 
 p1GuessBtn.onclick = p1Submit;
 p2GuessBtn.onclick = p2Submit;
 window.requestAnimationFrame(gameLoop);
 
-
 let game = new Game();
 
+// //add player one ships
+// game.playerOne.placeShip(game.playerOne.fleet[0], 0, 0);
+// game.playerOne.placeShip(game.playerOne.fleet[1], 4, 2);
+// game.playerOne.placeShip(game.playerOne.fleet[2], 6, 3);
+// game.playerOne.placeShip(game.playerOne.fleet[3], 2, 7);
+// game.playerOne.placeShip(game.playerOne.fleet[4], 8, 9);
 
-//add player one ships
-game.playerOne.placeShip(game.playerOne.fleet[0], 0, 0);
-game.playerOne.placeShip(game.playerOne.fleet[1], 4, 2);
-game.playerOne.placeShip(game.playerOne.fleet[2], 6, 3);
-game.playerOne.placeShip(game.playerOne.fleet[3], 2, 7);
-game.playerOne.placeShip(game.playerOne.fleet[4], 8, 9);
-
-//add player two ships
+// //add player two ships
 game.playerTwo.placeShip(game.playerTwo.fleet[0], 0, 0);
 game.playerTwo.placeShip(game.playerTwo.fleet[1], 6, 4);
 game.playerTwo.placeShip(game.playerTwo.fleet[2], 2, 6);
-
-
 drawText();
-
-
-
